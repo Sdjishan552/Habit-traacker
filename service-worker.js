6fa62f55d5b53a27d48d846b31bd1511faaf2abe
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-04-26 11:09:03 +0530

    Update service worker version to v4.15

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  * ─────────────────────────────────────────────────────────────
  */
 
-const APP_VERSION = "v4.12;                     // ← ONLY change this when you update
+const APP_VERSION = "v4.15;                     // ← ONLY change this when you update
 const CACHE_NAME  = "discipline-tracker-v4-0";  // ← keep in sync with APP_VERSION
 
 const ASSETS = [
